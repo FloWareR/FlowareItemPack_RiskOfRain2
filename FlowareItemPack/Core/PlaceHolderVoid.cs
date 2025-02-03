d33a1259a04a61c3706922add41dfdfdacd96873
FLOWARE <rafaxtz3@gmail.com>
2025-02-02 21:14:16 -0600

    Updated void Template

--- FlowareItemPack/Core/PlaceHolderVoid.cs
+++ FlowareItemPack/Core/PlaceHolderVoid.cs
@@ -1,78 +1,94 @@
-﻿using R2API;
+﻿using HarmonyLib;
+using On.RoR2.Items;
+using R2API;
 using RoR2;
+using RoR2.ExpansionManagement;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+
+
 
 namespace FlowareItemPack.Items
 {
-    public class PlaceHolderVoid : BaseItem
+    internal class PlaceholderVoid : BaseItem
     {
         public override ItemDef ItemDef { get; } = ScriptableObject.CreateInstance<ItemDef>();
+
 
         public override void Initialize()
         {
             var assets = new Core.Assets();
-            assets.PopulateAssets("matches", "matches");
+            assets.PopulateAssets("", "");
 
-            ItemDef.name = "void_matches";
-            ItemDef.nameToken = "FLOWARE_VOIDMATCHES_NAME";
-            ItemDef.pickupToken = "FLOWARE_VOIDMATCHES_PICKUP";
-            ItemDef.descriptionToken = "FLOWARE_VOIDMATCHES_DESC";
-            ItemDef.loreToken = "FLOWARE_VOIDMATCHES_LORE";
+            ItemDef.name = "";
+            ItemDef.nameToken = "FLOWARE_DISPERSEOBSIDIAN_NAME";
+            ItemDef.pickupToken = "FLOWARE_DISPERSEOBSIDIAN_PICKUP";
+            ItemDef.descriptionToken = "FLOWARE_DISPERSEOBSIDIAN_DESC";
+            ItemDef.loreToken = "FLOWARE_DISPERSEOBSIDIAN_LORE";
 
-            LanguageAPI.Add("FLOWARE_VOIDMATCHES_NAME", "");
-            LanguageAPI.Add("FLOWARE_VOIDMATCHES_PICKUP", "");
-            LanguageAPI.Add("FLOWARE_VOIDMATCHES_DESC", "");
-            LanguageAPI.Add("FLOWARE_VOIDMATCHES_LORE", "");
+            LanguageAPI.Add("FLOWARE_DISPERSEOBSIDIAN_NAME", "");
+            LanguageAPI.Add("FLOWARE_DISPERSEOBSIDIAN_PICKUP", " <color=#ed7fcd>Corrupts all  </color>");
+            LanguageAPI.Add("FLOWARE_DISPERSEOBSIDIAN_DESC", " <color=#ed7fcd>Corrupts all  </color>");
+            LanguageAPI.Add("FLOWARE_DISPERSEOBSIDIAN_LORE", "");
 
-            // Set item tier to Void Tier 1
             ItemDef.tier = ItemTier.VoidTier1;
-
-            // Load the Void Tier 1 definition
-#pragma warning disable Publicizer001
             ItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/DLC1/Common/VoidTier1Def.asset").WaitForCompletion();
-#pragma warning restore Publicizer001
-
-            // Assign assets
-            ItemDef.pickupIconSprite = assets.icon;
-            ItemDef.pickupModelPrefab = assets.prefab;
             ItemDef.tags = new ItemTag[] { };
 
-            // Item settings
+            ItemDef.pickupIconSprite = assets.icon;
+            ItemDef.pickupModelPrefab = assets.prefab;
+
             ItemDef.canRemove = true;
             ItemDef.hidden = false;
 
-            // Register item with R2API
-            var displayRules = new ItemDisplayRuleDict(null);
-            ItemAPI.Add(new CustomItem(ItemDef, displayRules));
-
-            // Register the item with the Void item pool
-            if (ItemTierCatalog.availability.available)
-            {
-                ItemTierDef voidTierDef = ItemTierCatalog.GetItemTierDef(ItemTier.VoidTier1);
-                if (voidTierDef != null)
-                {
-                    voidTierDef.dropletDisplayPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/Common/VoidItemPickup.prefab").WaitForCompletion();
-                }
-            }
-
-            // Hook into events
+            ItemAPI.Add(new CustomItem(ItemDef, new ItemDisplayRuleDict(null)));
             Hook();
         }
 
+
         public override void Hook()
         {
+            On.RoR2.CharacterMaster.OnInventoryChanged += OnInventoryChanged;
+            On.RoR2.HealthComponent.TakeDamage += OnDamageDealt;
+            ContagiousItemManager.Init += SetTransformation;
 
         }
 
         public override void Unhook()
         {
+            On.RoR2.CharacterMaster.OnInventoryChanged -= OnInventoryChanged;
+            On.RoR2.HealthComponent.TakeDamage -= OnDamageDealt;
+            ContagiousItemManager.Init -= SetTransformation;
 
         }
 
-        private void OnServerDamageDealt(DamageReport report)
+        private void SetTransformation(ContagiousItemManager.orig_Init orig)
         {
-          
+
+            var dlcType = ExpansionCatalog.expansionDefs[0];
+            ItemDef.requiredExpansion = dlcType;
+
+            ItemDef.Pair transformation = new ItemDef.Pair()
+            {
+                itemDef1 = RoR2Content.Items.NearbyDamageBonus,
+                itemDef2 = ItemDef
+            };
+            ItemCatalog.itemRelationships[DLC1Content.ItemRelationshipTypes.ContagiousItem] =
+                ItemCatalog.itemRelationships[DLC1Content.ItemRelationshipTypes.ContagiousItem].AddToArray(transformation);
+            orig();
+        }
+        private void OnInventoryChanged(On.RoR2.CharacterMaster.orig_OnInventoryChanged orig, CharacterMaster self)
+        {
+            orig(self);
+
+            if (!self || !self.inventory) return;
+
+        }
+
+
+        private void OnDamageDealt(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
 
         }
     }
